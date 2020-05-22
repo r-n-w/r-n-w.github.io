8709d4c96ec62b475eedfa697f3dc0dfe59fa316
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-05-22 16:51:06 -0400

    test

--- pull.php
+++ pull.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 if ( $_POST['payload'] ) {
-shell_exec( ‘cd /home4/ryannath/auto_website.git/ && git reset –hard HEAD && git pull’ );
+shell_exec( ‘cd /home4/ryannath/auto_website.git' && 'git pull origin master' );
 }
 
 ?>hi
